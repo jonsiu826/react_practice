bb414e3a6c6ff67d86628d382301e6a5e3cbf44a
jsiu <jonsiu826@gmail.com>
2021-02-01 15:06:56 -0800

    fetched info from api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { Component } from 'react';
+import React ,{ Component } from 'react';
 
 class App extends Component {
   constructor(){
     super();
 
     this.state={
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: '123'
-        },
-        {
-          name: 'Dracula',
-          id: '124'
-        },
-        {
-          name: 'Zombie',
-          id: '125'
-        }
-      ]
-  }
+      monsters: []
+    }
 }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
   render(){
     return (
       <div className="App">
